ef7bcc492d58b21bae78f7e505dfb8e940f060a5
Ayush Saini <ayushsaini8697@gmail.com>
2020-09-24 13:26:24 +0530

    added TheHindu
    one more site added

--- webscrapper/thescrapper.js
+++ webscrapper/thescrapper.js
@@ -29,11 +29,12 @@ $('.td-module-thumb a').each((i,elem)=>{
   pagetofetch[url]={'domain':'beebom','type':'tech'};
 });
   await driver.quit();
-
-//console.log($('.td-module-thumb a'));
+console.log(pagetofetch);
+//console.log($('.td-module-thumb a'));000
 }
 
-//==============================================================================
+//fetchBeebom();
+//===================================ANI===========================================
 async function fetchANI_National() {
   await driver.get('https://aninews.in/category/national/');
   html = await driver.getPageSource();
@@ -86,12 +87,50 @@ $('.extra-related-block .bottom figcaption a').each((i,elem)=>{
   await driver.quit();
 }
 
-//==============================================================================
+//================================== The Hindu========================================
+
+async function fetchTheHindu() {
+  await driver.get('https://www.thehindu.com/latest-news/');
+  html = await driver.getPageSource();
+   $ = cheerio.load(html);
+ 
+$('.latest-news li a').each((i,elem)=>{
+ // pagetofetch.push(elem.attribs.href);
+  url=elem.attribs.href;
+  route=url.split("/");
+  if(route[3]==="news"){
+        if(route[4]==="national"){
+          pagetofetch[url]={'domain':'thehindu','type':'national'};
+        }
+        else if(route[4]==="international"){
+          pagetofetch[url]={'domain':'thehindu','type':'national'};
+        }
+        else if(route[4]==="cities"){
+          pagetofetch[url]={'domain':'thehindu','type':'cities','subtype':route[5].toLowerCase()};  //city name
+        }
+  }
+  else if(route[3]==="business"){
+    pagetofetch[url]={'domain':'thehindu','type':'business'};
+  }
+  else if(route[3]==="entertainment"){
+    pagetofetch[url]={'domain':'thehindu','type':'entertainment'};
+  }
+  else if(route[3]==="sci-tech"){
+    pagetofetch[url]={'domain':'thehindu','type':'sci-tech'};
+  }
+  else if(route[3]==="books"){
+    pagetofetch[url]={'domain':'thehindu','type':'books'};
+  }
+  else if(route[3]==="sport"){
+    pagetofetch[url]={'domain':'thehindu','type':'sport','subtype':route[4].toLowerCase()};
+  }
+});
+  await driver.quit();
+  //console.log(pagetofetch);
+}
 
 
-
-
-
+fetchTheHindu();
 
 
 function pagefetcher(){
