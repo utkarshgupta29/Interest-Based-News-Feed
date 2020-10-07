d9aba42ed6cbcd7bd8fbaf5aca0faf9cd2ba6c7c
Ayush Saini <ayushsaini8697@gmail.com>
2020-10-08 00:33:27 +0530

    custom search for ANI

--- webscrapper/customsearch.js
+++ webscrapper/customsearch.js
@@ -1,6 +1,6 @@
 /*
  Input: search(keyword,target)    keyword: word to search , target: website name to search on
- Output: list of links of matching article    
+ Output: list of objects of matching article  with title and thumbnail  
 */
 
 
@@ -26,6 +26,10 @@ const driver = new webdriver.Builder().forBrowser('firefox').setFirefoxOptions(n
 async function search(keyword,target){
 	if(target==='AajTak')
 		 return await searchAajTak(keyword);
+  if (target==='beebom')
+    return await searchBeebom(keyword);
+  if (target==='ani')
+    return await searchAni(keyword);
 
 }
 
@@ -56,7 +60,64 @@ async function searchAajTak(keyword){
  	return posts;
  }
 
-//=========== driver  for testing ==========
+//============================= beebom  ==================
+//              site side issue taking too long to render results  #WILLDOLATER
+async function searchBeebom(keyword){
+  var posts ={};
+   var val = keyword.trim();                       
+   var key = encodeURIComponent(val);
+   if (key.length != 0) {
+      URL = "https://beebom.com/?s=" + key
+      }
+  await driver.get(URL);
+
+    html = await driver.getPageSource();
+     $ = cheerio.load(html);
+     $("#datafetch .search-list").each((i,elem)=>{
+ 
+     
+       data=elem
+       console.log(data);
+      // posts[url]={'title':title,'image':image,'date':date};
+      
+  });
+ //await driver.quit();
+  return posts;
+ }
+
+//==================
+async function searchAni(keyword){
+  var posts ={};
+   var val = keyword.trim();                       
+   var key = encodeURIComponent(val);
+   if (key.length != 0) {
+      URL = "https://aninews.in/search/?query=" + key
+      }
+  await driver.get(URL);
+//await driver.findElement(By.css("body")).sendKeys(Key.chord(Keys.CONTROL, Keys.END));  //was taking to long to load image and data of lower articles/cards ... hope scrolling will take some time and help loading
+// just hit and try  
+//rip it not worked ; figured alternative way
+    html = await driver.getPageSource();
+     $ = cheerio.load(html);
+     $(".extra-news-block .card").each((i,elem)=>{
+ 
+     
+       image=elem.children[1].children[1].children[1].attribs['data-src'];
+       url=elem.children[1].children[3].children[1].attribs.href;
+       title=elem.children[1].children[3].children[1].children[1].children[0].data;
+       time=elem.children[1].children[3].children[3].children[1].children[0].children[0].data + elem.children[1].children[3].children[3].children[1].children[1].data;
+       
+       posts[url]={'title':title,'image':image,'date':time};
+      
+  });
+await driver.quit();
+  return posts;
+ }
+
+
+
+=========== driver  for testing ==========
 search('rape','AajTak').then((data)=>{
 console.log(data);
 });
+
