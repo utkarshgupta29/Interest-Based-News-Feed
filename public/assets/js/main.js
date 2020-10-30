10847f8ddb21a779460974ddd9c28b6745103e19
Ayush Saini <ayushsaini8697@gmail.com>
2020-10-30 17:53:34 +0530

    search button removed

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -37,3 +37,11 @@ function func(e){
         
     })
 }
+
+$("#searchbox").on('keyup', function (e) {
+    if (e.key === 'Enter' || e.keyCode === 13) {
+        $("#searchform").submit();
+    }
+});
+
+
