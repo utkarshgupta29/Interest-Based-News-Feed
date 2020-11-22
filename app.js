06ec5510e56620b24145ebec0d8c48cb59722c09
utkarshgupta29 <42411771+utkarshgupta29@users.noreply.github.com>
2020-11-22 23:45:50 +0530

    trial 3

--- app.js
+++ app.js
@@ -253,6 +253,6 @@ function isLoggedIn(req,res,next){
 	//console.log("incorrect password");
 	res.redirect("/login");
 }
-app.listen(5000||process.env.PORT,process.env.IP,function(){
+app.listen(process.env.PORT || 5000,process.env.IP,function(){
 console.log("server started");
 });
